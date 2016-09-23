45f1049661387433354d7220b763277d9cac061a
rich-farr <scarletmanuka@gmail.com>
2016-09-23 14:49:27 +1200

    Add links to Home component

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 class Home extends React.Component {
   render() {
 		return (
 		  <div id="home">
         <h2>This is the Home component</h2>
+        <div id="home-links">
+          <Link to="/about" activeClassName="active">About</Link>
+          <Link to="/resume" activeClassName="active">Resume</Link>
+          <Link to="/projects" activeClassName="active">Projects</Link>
+          <a href="https://nz.linkedin.com/in/rich-farr-web-dev" target="_blank">LinkedIn</a>
+          <a href="https://github.com/rich-farr" target="_blank">Github</a>
+          <a href="#" target="_blank">Email</a>
+        </div>
 		  </div>
 		)
 	}
