87d100d8858f24879d244c6ee9ff4bc6ec106e9e
rich-farr <scarletmanuka@gmail.com>
2016-09-23 14:15:20 +1200

    Set up home component and get it to render on the page

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends React.Component {
+  render() {
+		return (
+		  <div id="home">
+        <h2>This is the Home component</h2>
+		  </div>
+		)
+	}
+}
+
+export default Home
