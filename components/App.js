dea40ecfbca27b751481955d11a3987cefcbda28
rich-farr <scarletmanuka@gmail.com>
2016-09-23 14:11:14 +1200

    Create component files, set routes, import components into index.js

--- components/App.js
+++ components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import Navbar from './navbar'
+import Nav from './Nav'
 
 class App extends React.Component {
   render() {
 		return (
 		  <div id="container">
-		    <Navbar title="Rich Farr, W.D."/>
+		    <Nav title="Rich Farr, W.D."/>
 					{this.props.children}
 		  </div>
 		)
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import App from './components/App'
+import Home from './components/Home'
+import About from './components/About'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
 
-render(<App name='Personal site built with react' />, document.querySelector('main'))
-console.log('welcome to Personal site built with react')
+render((
+	<Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home}/>
+	    <Route path='/about' component={About}/>
+      <Route path='/resume' component={Resume}/>
+      <Route path='/projects' component={Projects}/>
+    </Route>
+  </Router>
+  ),
+  document.querySelector('main')
+)
+
+console.log('welcome to my new Personal site built with react')
