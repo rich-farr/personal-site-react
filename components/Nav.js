266306932a70324442154e7bbc934c75af881667
rich-farr <scarletmanuka@gmail.com>
2016-09-23 14:28:37 +1200

    Add links to Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { IndexLink, Link } from 'react-router'
 
-class Navbar extends React.Component {
+class Nav extends React.Component {
   render() {
 		return (
-		  <div id="navbar">
-		    <h1>{this.props.title}</h1>
-		  </div>
+      <nav>
+        <div id="nav-heading">
+          <h1>{this.props.title}</h1>
+        </div>
+        <div id="nav-links">
+          <IndexLink to="/" activeClassName="active">Home</IndexLink>
+          <Link to="/about" activeClassName="active">About</Link>
+          <Link to="/resume" activeClassName="active">Resume</Link>
+          <Link to="/projects" activeClassName="active">Projects</Link>
+        </div>
+      </nav>
 		)
 	}
 }
 
-export default Navbar
+export default Nav
