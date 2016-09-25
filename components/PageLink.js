a246260445b2f5443f8cb08a481875a898312c31
rich-farr <scarletmanuka@gmail.com>
2016-09-26 12:15:42 +1300

    Create PageLink component

---
+++ components/PageLink.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class PageLink extends React.Component {
+  render() {
+		return (
+		  <div id="page-link">
+        <h2>This is a Link component</h2>
+		  </div>
+		)
+	}
+}
+
+export default PageLink
