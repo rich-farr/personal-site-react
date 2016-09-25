55b3eeb00173206896d68321ec131218b70031eb
rich-farr <scarletmanuka@gmail.com>
2016-09-26 12:56:01 +1300

    Return different PageLink depending on value of props.is_url

--- components/PageLink.js
+++ components/PageLink.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-class PageLink extends React.Component {
-  render() {
-		return (
-		  <div id="page-link">
-        <h2>This is a Link component</h2>
-		  </div>
-		)
-	}
+let PageLink = (props) => {
+  console.log("MOO", props)
+  if (props.is_url) {
+  	return (
+  	  <div class="page-link">
+        <h2>This is an external Link component</h2>
+  	  </div>
+  	)
+  } else {
+    return (
+      <div class="page-link">
+        <h2>This is an internal Link component</h2>
+      </div>
+    )
+  }
 }
 
 export default PageLink
+
+// let Book = (props) => {
+//   let linkTo = "/books/" + props.id
+//   return (
+//     <div className="book">
+//       <div className="cover-image">
+//         <img src={props.image} />
+//       </div>
+//       <div className="book-content">
+//         <h1><Link to={linkTo}>{props.title} ({props.year})</Link></h1>
+//         <h3>{props.author}</h3>
+//       </div>
+//     </div>
+//     )
+// }
