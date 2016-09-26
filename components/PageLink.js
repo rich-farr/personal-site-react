90793efa0213a9126c23c558da87f9cb80e1838e
rich-farr <scarletmanuka@gmail.com>
2016-09-26 13:13:12 +1300

    Complete PageLink component; all rendering properly inside Home component

--- components/PageLink.js
+++ components/PageLink.js
@@ -2,35 +2,19 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 
 let PageLink = (props) => {
-  console.log("MOO", props)
   if (props.is_url) {
   	return (
-  	  <div class="page-link">
-        <h2>This is an external Link component</h2>
+  	  <div className="home-link" id={props.value.toLowerCase() + "-link"}>
+        <a href={props.path} target="_blank">{props.value}</a>
   	  </div>
   	)
   } else {
     return (
-      <div class="page-link">
-        <h2>This is an internal Link component</h2>
+      <div className="home-link" id={props.path + "-link"}>
+        <Link to={"/" + props.path} activeClassName="active">{props.value}</Link>
       </div>
     )
   }
 }
 
 export default PageLink
-
-// let Book = (props) => {
-//   let linkTo = "/books/" + props.id
-//   return (
-//     <div className="book">
-//       <div className="cover-image">
-//         <img src={props.image} />
-//       </div>
-//       <div className="book-content">
-//         <h1><Link to={linkTo}>{props.title} ({props.year})</Link></h1>
-//         <h3>{props.author}</h3>
-//       </div>
-//     </div>
-//     )
-// }
